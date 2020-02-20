f263da73aea0b6da3525bf6dbec2666a6c5d7262
Lisa <mobile.appstone@gmail.com>
2020-02-20 18:46:56 +0530

    //relative layout

--- layout/src/main/java/com/example/layout/MainActivity.java
+++ layout/src/main/java/com/example/layout/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.layout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_relative);
+
+        mBtn = findViewById(R.id.btn_login);
+        EditText mEtUserID = findViewById(R.id.et_user_id);
+
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Toast.makeText(this, "dfghj", Toast.LENGTH_SHORT).show();
     }
+
+
 }
